c696457ba37e782ad565097a2f354ae491bb70ef
Willdenberg Ferreira viana <willdenbergviana@outlook.com>
2020-05-28 00:25:37 -0300

    Codigo html e JS

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+var valor, resultado;
+
+function botao(num){
+    valor = document.calc.visor.value += num;
+}
+function reset(){
+    document.calc.visor.value = '';
+}
+function calcula(){
+    resultado = eval(valor);
+    document.calc.visor. value = resultado
+}
